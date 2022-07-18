0e33dd01717889ba9a00d900e3de46753a71c491
Dwightu <yudehui1115@gmail.com>
2022-07-18 12:00:25 -0400

    Add user Redux

--- src/index.js
+++ src/index.js
@@ -4,9 +4,10 @@ import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom'
-import { UserProvider } from './contexts/user.context';
 import { CategoriesProvider } from './contexts/categories.context';
 import { CartProvider } from './contexts/cart.context';
+import { Provider } from 'react-redux';
+import { store } from './store/store';
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
@@ -14,15 +15,17 @@ const root = createRoot(rootElement);
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <UserProvider>
+    <Provider store={store}>
+      <BrowserRouter>
+        {/* <UserProvider> */}
         <CategoriesProvider>
           <CartProvider>
             <App />
           </CartProvider>
         </CategoriesProvider>
-      </UserProvider>
-    </BrowserRouter>
+        {/* </UserProvider> */}
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
 );
 
--- src/utils/reducer/reducer.utils.js
+++ src/utils/reducer/reducer.utils.js
@@ -1,3 +1,2 @@
-import { type } from "@testing-library/user-event/dist/type"
 
-export const createAction = (type, payload) => { type, payload }
+export const createAction = (type, payload) => ({ type, payload })
